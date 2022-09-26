2aa5fa2749488e0c6b5ad1cccab068ec1603138b
Hannah Brooks <hannah.brooks@mail.utoronto.ca>
2022-09-26 17:45:48 -0400

    added email field

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, StopValidation
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -13,6 +13,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
@@ -30,10 +31,24 @@ def user(name):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    email_reminder = False
+
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None:
+            if 'utoronto' not in form.email.data:
+                email_reminder = True
+            if old_email != form.email.data:
+                flash('Looks like you have changed your email!')            
+
         session['name'] = form.name.data
+        session['email'] = form.email.data
+        session['email_reminder'] = email_reminder
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+
+    return render_template('index.html', form=form, name=session.get('name'), email = session.get('email'), email_reminder = session.get('email_reminder'))
